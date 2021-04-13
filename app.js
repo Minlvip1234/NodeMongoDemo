667f15d068ec104246680ffae0445e504a621508
Minh <anhddgch18611@fpt.edu.vn>
2021-04-13 14:50:51 +0700

    commit

--- app.js
+++ app.js
@@ -83,6 +83,6 @@ app.get('/edit',async(req,res)=>{
     res.render('edit',{product: productToEdit})
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 app.listen(PORT);
 console.log('Server is running at 3000')
